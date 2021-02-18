e3a32dad04ae72074c1a28fa2adb44f9aad61b3a
Julia Novak <julia.novak9@gmail.com>
2021-02-18 13:12:10 +0200

    finished UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  ButtonGroup,
+  ButtonOr,
+  Container,
+  Form,
+  FormGroup,
+  FormInput,
+  Grid,
+  GridColumn,
+  GridRow,
+  Header,
+  Icon,
+  Segment,
+  Statistic,
+  StatisticLabel,
+  StatisticValue,
+} from 'semantic-ui-react'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Header as='h1'>Budget</Header>
+
+      <Statistic size='small'>
+        <StatisticLabel>Your Balance:</StatisticLabel>
+        <StatisticValue>2,550.53</StatisticValue>
+      </Statistic>
+
+      <Segment textAlign='center'>
+        <Grid columns={2} divided>
+          <GridRow>
+            <GridColumn>
+              <Statistic size='tiny' color='green'>
+                <StatisticLabel textAlign='left'>Income:</StatisticLabel>
+                <StatisticValue>1,045.50</StatisticValue>
+              </Statistic>
+            </GridColumn>
+            <GridColumn>
+              <Statistic size='tiny' color='red'>
+                <StatisticLabel textAlign='left'>Expenses:</StatisticLabel>
+                <StatisticValue>6000</StatisticValue>
+              </Statistic>
+            </GridColumn>
+          </GridRow>
+        </Grid>
+      </Segment>
+
+      <Header as='h3'>History</Header>
+
+      <Segment color='red'>
+        <Grid columns={3} textAlign='right'>
+          <GridRow>
+            <GridColumn width={10} textAlign='left'>
+              Something
+            </GridColumn>
+            <GridColumn width={3} textAlign='right'>
+              $10.00
+            </GridColumn>
+            <GridColumn width={3}>
+              <Icon name='edit' bordered />
+              <Icon name='trash' bordered />
+            </GridColumn>
+          </GridRow>
+        </Grid>
+      </Segment>
+
+      <Segment color='red'>
+        <Grid columns={3} textAlign='right'>
+          <GridRow>
+            <GridColumn width={10} textAlign='left'>
+              Something else
+            </GridColumn>
+            <GridColumn width={3} textAlign='right'>
+              $100.00
+            </GridColumn>
+            <GridColumn width={3}>
+              <Icon name='edit' bordered />
+              <Icon name='trash' bordered />
+            </GridColumn>
+          </GridRow>
+        </Grid>
+      </Segment>
+
+      <Segment color='red'>
+        <Grid columns={3} textAlign='right'>
+          <GridRow>
+            <GridColumn width={10} textAlign='left'>
+              Something
+            </GridColumn>
+            <GridColumn width={3} textAlign='right'>
+              $20.00
+            </GridColumn>
+            <GridColumn width={3}>
+              <Icon name='edit' bordered />
+              <Icon name='trash' bordered />
+            </GridColumn>
+          </GridRow>
+        </Grid>
+      </Segment>
+
+      <Header as='h3'>Add new transaction</Header>
+      <Form unstackable>
+        <FormGroup>
+          <FormInput
+            icon='tags'
+            width={12}
+            label='Description'
+            placeholder='New shiny thing'
+          />
+          <FormInput
+            width={4}
+            label='Value'
+            placeholder='100.00'
+            icon='dollar'
+            iconPosition='left'
+          ></FormInput>
+        </FormGroup>
+        <ButtonGroup style={{ marginTop: 20 }}>
+          <Button>Cancel</Button>
+          <ButtonOr />
+          <Button primary>Ok</Button>
+        </ButtonGroup>
+      </Form>
+    </Container>
+  )
 }
 
-export default App;
+export default App
